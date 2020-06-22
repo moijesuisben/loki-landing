43154645eb566cd849f41d5c39bd739ac5dd70ad
moijesuisben <benji.apg@gmail.com>
2020-06-23 01:29:43 +0200

    Updates

---
+++ precache-manifest.305d4e6135463084e65e9bfcbb38d073.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1231dbb77417b6c9a08aad3429e4ada",
+    "url": "/loki-landing/index.html"
+  },
+  {
+    "revision": "d067c2efedf467269493",
+    "url": "/loki-landing/static/css/main.d695be62.chunk.css"
+  },
+  {
+    "revision": "b84c7fb6bb5d803484f8",
+    "url": "/loki-landing/static/js/2.2c19c40e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/loki-landing/static/js/2.2c19c40e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d067c2efedf467269493",
+    "url": "/loki-landing/static/js/main.b64ac66d.chunk.js"
+  },
+  {
+    "revision": "a6d305d4729c33140317",
+    "url": "/loki-landing/static/js/runtime-main.efc1e157.js"
+  },
+  {
+    "revision": "7fd44d83d2f618696ae031fc175f00f6",
+    "url": "/loki-landing/static/media/BackgroundContact.7fd44d83.png"
+  },
+  {
+    "revision": "b507f85ca584462fa63838b7ee7049d1",
+    "url": "/loki-landing/static/media/aleop.b507f85c.png"
+  },
+  {
+    "revision": "a2b9bef4dea4eadfcaa242b915173937",
+    "url": "/loki-landing/static/media/background.a2b9bef4.png"
+  },
+  {
+    "revision": "67f6f3569acabf701e0eed86895930f5",
+    "url": "/loki-landing/static/media/backgroundAbout.67f6f356.png"
+  },
+  {
+    "revision": "1ab2182d9102f514796780d4ab4fd3a9",
+    "url": "/loki-landing/static/media/bicloo.1ab2182d.png"
+  },
+  {
+    "revision": "57140fe257b9f70cdca5eddf77a59552",
+    "url": "/loki-landing/static/media/cunia.57140fe2.woff"
+  },
+  {
+    "revision": "debdcb955da001cfd03d880710caed3a",
+    "url": "/loki-landing/static/media/cunia.debdcb95.woff2"
+  },
+  {
+    "revision": "da5c8566f25b0ba0b638d74e028a602a",
+    "url": "/loki-landing/static/media/frenchTech.da5c8566.png"
+  },
+  {
+    "revision": "03ef99d40ca594b16382e30f4cfd9689",
+    "url": "/loki-landing/static/media/heetch.03ef99d4.png"
+  },
+  {
+    "revision": "152f372555ce0bb4c034cb50bce09929",
+    "url": "/loki-landing/static/media/jcdecaux.152f3725.png"
+  },
+  {
+    "revision": "7ffc1d952093b0930dd0db4bada419e7",
+    "url": "/loki-landing/static/media/lime.7ffc1d95.png"
+  },
+  {
+    "revision": "f38383a7a32840bef3ceeac3e230a27e",
+    "url": "/loki-landing/static/media/logo.f38383a7.png"
+  },
+  {
+    "revision": "3bfef63c4c3f874d23cc35c87821d6a6",
+    "url": "/loki-landing/static/media/marguerite.3bfef63c.png"
+  },
+  {
+    "revision": "ba4609626a42f8dcfbe6cd4356d7e4e7",
+    "url": "/loki-landing/static/media/ministere.ba460962.png"
+  },
+  {
+    "revision": "b0bdcef805f22de4f6cca82204646001",
+    "url": "/loki-landing/static/media/mobile.b0bdcef8.png"
+  },
+  {
+    "revision": "392b59b4678a8da6d3fefb66bb09b684",
+    "url": "/loki-landing/static/media/mobileDots.392b59b4.png"
+  },
+  {
+    "revision": "de1e9566bf7f86f825f69659c297bd45",
+    "url": "/loki-landing/static/media/sncf.de1e9566.png"
+  },
+  {
+    "revision": "514078a59bde9a165e48424eeb0a1fa8",
+    "url": "/loki-landing/static/media/star.514078a5.png"
+  },
+  {
+    "revision": "0ad40bad823e696c4580600991509694",
+    "url": "/loki-landing/static/media/tan.0ad40bad.png"
+  },
+  {
+    "revision": "ee430489d1505483166c19ab9ed00d4e",
+    "url": "/loki-landing/static/media/uber.ee430489.png"
+  }
+]);
