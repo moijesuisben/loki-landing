36f25db1c1faeee0314b0d87d20e5ab848fe497d
moijesuisben <benji.apg@gmail.com>
2020-06-22 20:42:16 +0200

    [WIP] Header added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,78 @@
 import React from "react";
 import { createUseStyles } from "react-jss";
 import * as variables from "../variables";
+import background from "../media/background.png";
+import mobile from "../media/mobile.png";
+import logo from "../media/logo.png";
+import ios from "../media/ios.png";
+import android from "../media/android.png";
 
 const useStyles = createUseStyles({
-    header :{
-        border: '1px dotted lightblue',
-    }
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  download: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: "calc(50px + (150 - 50) * ((100vw - 320px) / (1920 - 320)))",
+    "& > img": {
+      maxWidth: "calc(50px + (332 - 50) * ((100vw - 320px) / (1920 - 320)))",
+    },
+    "& > img:hover": {
+      cursor: "pointer",
+    },
+    "& > p": {
+      fontSize: "23px",
+      fontWeight: 800,
+    },
+  },
+  mobile: {
+    maxHeight: "calc(100px + (714 - 100) * ((100vw - 320px) / (1920 - 320)))",
+  },
+  logo: {
+    maxWidth: "calc(50px + (287 - 50) * ((100vw - 320px) / (1920 - 320)))",
+  },
+  text: {
+    fontSize: 17,
+    maxWidth: "calc(50px + (466 - 50) * ((100vw - 320px) / (1920 - 320)))",
+    fontWeight: 600,
+    lineHeight: 1.5,
+    letterSpacing: "0.2px",
+  },
+  background: {
+    position: "absolute",
+    top: "-310px",
+    left: 0,
+    width: "1920px",
+    height: "auto",
+    zIndex: -1,
+  },
 });
 
 export default function Header() {
   const classes = useStyles();
   return (
-      <div className={classes.header}>
-          <h1>Header</h1>
+    <div className={classes.header}>
+      <div className={classes.download}>
+        <img src={ios} alt="ios" />
+        <p>ou</p>
+        <img src={android} alt="android" />
       </div>
+      <img src={mobile} alt="phone" className={classes.mobile} />
+      <div className={classes.presentation}>
+        <img src={logo} alt="logo" className={classes.logo} />
+        <p className={classes.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris
+          rhoncus aenean vel elit scelerisque mauris. Integer enim neque
+          volutpat ac tincidunt. Pretium viverra suspendisse potenti nullam ac.
+          Accumsan tortor posuere ac ut consequat.
+        </p>
+      </div>
+      <img src={background} alt="background" className={classes.background} />
+    </div>
   );
 }
--- src/views/Landing.js
+++ src/views/Landing.js
@@ -4,19 +4,25 @@ import * as variables from "../variables";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-const useStyles = createUseStyles({});
+const useStyles = createUseStyles({
+  container: {
+    paddingTop: "calc(50px + (150 - 50) * ((100vw - 320px) / (1920 - 320)))",
+    marginLeft: "calc(20px + (180 - 20) * ((100vw - 320px) / (1920 - 320)))",
+    marginRight: "calc(20px + (180 - 20) * ((100vw - 320px) / (1920 - 320)))",
+  },
+});
 
 export default function Landing() {
   const classes = useStyles();
   return (
-    <>
+    <div className={classes.container}>
       <Header />
-      <p>
+      {/* <p>
         Ipsum sint ad dolore dolor commodo elit officia. Elit ullamco
         adipisicing elit aute mollit reprehenderit est id. Laboris culpa
         proident sint minim aute sint do. Proident labore quis laborum elit.
       </p>
-      <Footer />
-    </>
+      <Footer /> */}
+    </div>
   );
 }
